ec05d1089b2890fb42a6397d474c2325f143b214
Florent Pled <florent.pled@univ-paris-est.fr>
2018-05-05 20:19:24 +0200

    Added example portalFrame.m

--- examples/plate/portalFrame.m
+++ examples/plate/portalFrame.m
@@ -1,5 +1,5 @@
-%% Portique plan hyperstatique de degré 3 %%
-%%----------------------------------------%%
+%% Portal frame %%
+%%--------------%%
 
 % clc
 clearvars
@@ -26,16 +26,16 @@ b2 = 500;
 h = 15;
 
 % Points
-P1_beam1_left = POINT([0.0,0.0]);
-P2_beam1_left = POINT([0.0,L1]); % the same point of P1_beam2
+P1 = POINT([0.0,0.0]);
+P2 = POINT([0.0,L1]);
 P_load = POINT([L2/2,L1]);
-P1_beam1_right = POINT([L2,0.0]);
-P2_beam1_right = POINT([L2,L1]); % the same point of P2_beam2
+P3 = POINT([L2,0.0]);
+P4 = POINT([L2,L1]);
 
-L_beam{1} = LIGNE(P1_beam1_left, P2_beam1_left); % L_beam1_left
-L_beam{2} = LIGNE(P2_beam1_left, P_load);
-L_beam{3} = LIGNE(P_load, P2_beam1_right); % L_beam2
-L_beam{4} = LIGNE(P1_beam1_right, P2_beam1_right); % L_beam1_right
+L_beam{1} = LIGNE(P1,P2);
+L_beam{2} = LIGNE(P2,P_load);
+L_beam{3} = LIGNE(P_load,P4);
+L_beam{4} = LIGNE(P3,P4);
 
 cl_beam = h/2;
 S_beam = cellfun(@(L,n) build_model(L,'cl',cl_beam,'elemtype','BEAM','filename',fullfile(pathname,['gmsh_beam_' num2str(n) '_cl_' num2str(cl_beam)])),L_beam,num2cell(1:length(L_beam)),'UniformOutput',false);
@@ -60,12 +60,12 @@ IZ2 = b2*h^3/12;
 IX1 = IY1+IZ1;
 IX2 = IY2+IZ2;
 
-mat1_beam = ELAS_BEAM('E',E_beam,'NU',NU_beam,'S',Sec_beam1,'IZ',IZ1,'IY',IY1,'IX',IX1,'RHO',RHO_beam);
-mat1_beam = setnumber(mat1_beam,1);
-mat2_beam = ELAS_BEAM('E',E_beam,'NU',NU_beam,'S',Sec_beam2,'IZ',IZ2,'IY',IY2,'IX',IX2,'RHO',RHO_beam);
-mat2_beam = setnumber(mat2_beam,2);
-S_beam([1,4]) = cellfun(@(S) setmaterial(S,mat1_beam),S_beam([1,4]),'UniformOutput',false);
-S_beam([2,3]) = cellfun(@(S) setmaterial(S,mat2_beam),S_beam([2,3]),'UniformOutput',false);
+mat_beam{1} = ELAS_BEAM('E',E_beam,'NU',NU_beam,'S',Sec_beam1,'IZ',IZ1,'IY',IY1,'IX',IX1,'RHO',RHO_beam);
+mat_beam{1} = setnumber(mat_beam{1},1);
+mat_beam{2} = ELAS_BEAM('E',E_beam,'NU',NU_beam,'S',Sec_beam2,'IZ',IZ2,'IY',IY2,'IX',IX2,'RHO',RHO_beam);
+mat_beam{2} = setnumber(mat_beam{2},2);
+S_beam([1,4]) = cellfun(@(S) setmaterial(S,mat_beam{1}),S_beam([1,4]),'UniformOutput',false);
+S_beam([2,3]) = cellfun(@(S) setmaterial(S,mat_beam{2}),S_beam([2,3]),'UniformOutput',false);
 
 S = union(S_beam{:});
 
@@ -73,7 +73,7 @@ S = union(S_beam{:});
 p = 1;
 
 %% Dirichlet boundary conditions
-P_support = [P1_beam1_left P1_beam1_right];
+P_support = [P1 P3];
 S = final(S);
 S = addcl(S,P_support);
 
@@ -86,10 +86,38 @@ t = tic;
 u = A\f;
 time = toc(t);
 
-u = unfreevector(S,u);
-
 e = calc_epsilon(S,u,'smooth');
 s = calc_sigma(S,u,'smooth');
+
+%% Test solution
+N = s(1);
+Mz = s(2);
+N_max = max(abs(N));
+Mz_max = max(abs(Mz));
+
+[~,numnode2,~] = intersect(S,P2,'strict',false);
+N2 = 0;
+Mz2 = 0;
+for p=1:getnbgroupelem(S)
+    Np = reshape(abs(N{p}),[getnbnode(S),1]);
+    Mzp = reshape(abs(Mz{p}),[getnbnode(S),1]);
+    Np2 = double(Np(numnode2));
+    Mzp2 = double(Mzp(numnode2));
+    N2 = max(N2,Np2);
+    Mz2 = max(Mz2,Mzp2);
+end
+N = N2;
+Mz = Mz2;
+
+N_ex = 0;
+Mz_ex = 0;
+err_N = norm(N-N_ex)/norm(N_ex);
+err_Mz = norm(Mz-Mz_ex)/norm(Mz_ex);
+
+N_max_ex = 0;
+Mz_max_ex = 0;
+err_N_max = norm(N_max-N_max_ex)/norm(N_max_ex);
+err_Mz_max = norm(Mz_max-Mz_max_ex)/norm(Mz_max_ex);
 
 %% Outputs
 fprintf('\nCrossbar\n');
@@ -97,6 +125,20 @@ fprintf('nb elements = %g\n',getnbelem(S));
 fprintf('nb nodes    = %g\n',getnbnode(S));
 fprintf('nb dofs     = %g\n',getnbddl(S));
 fprintf('elapsed time = %f s\n',time);
+fprintf('\n');
+
+disp('Force N and moment Mz at point'); disp(P2);
+fprintf('N     = %g N\n',N);
+fprintf('N_ex  = %g N, error = %g\n',N_ex,err_N);
+fprintf('Mz    = %g N.mm\n',Mz);
+fprintf('Mz_ex = %g N.mm, error = %g\n',Mz_ex,err_Mz);
+fprintf('\n');
+
+disp('Maximum force N and moment Mz');
+fprintf('N_max     = %g N\n',N_max);
+fprintf('N_max_ex  = %g N, error = %g\n',N_max_ex,err_N_max);
+fprintf('Mz_max    = %g N.mm\n',Mz_max);
+fprintf('Mz_max_ex = %g N.mm, error = %g\n',Mz_max_ex,err_Mz_max);
 fprintf('\n');
 
 %% Display domains, boundary conditions and meshes
@@ -107,7 +149,7 @@ mymatlab2tikz(pathname,'domain.tex');
 [hD,legD] = plotBoundaryConditions(S,'legend',false);
 ampl = 300;
 [hN,legN] = vectorplot(S,'F',f,ampl,'r','LineWidth',1);
-hP = plot(P_load,'g+');
+hP = plot(P2,'g+');
 legend([hD,hN,hP],[legD,legN,'measure'],'Location','NorthEastOutside')
 mysaveas(pathname,'boundary_conditions',formats,renderer);
 
@@ -136,17 +178,18 @@ mysaveas(pathname,'u_y',formats,renderer);
 plotSolution(S,u,'displ',3,'ampl',ampl);
 mysaveas(pathname,'r_z',formats,renderer)
 
-plotSolution(S,u,'epsilon',1,'ampl',ampl);
-mysaveas(pathname,'eps_x',formats,renderer);
-
-plotSolution(S,u,'epsilon',2,'ampl',ampl);
-mysaveas(pathname,'gam_z',formats,renderer);
-
-plotSolution(S,u,'sigma',1,'ampl',ampl);
-mysaveas(pathname,'F_x',formats,renderer);
-
-plotSolution(S,u,'sigma',2,'ampl',ampl);
-mysaveas(pathname,'M_z',formats,renderer);
+% DO NOT WORK WITH BEAM ELEMENTS
+% plotSolution(S,u,'epsilon',1,'ampl',ampl);
+% mysaveas(pathname,'eps_x',formats,renderer);
+% 
+% plotSolution(S,u,'epsilon',2,'ampl',ampl);
+% mysaveas(pathname,'gam_z',formats,renderer);
+% 
+% plotSolution(S,u,'sigma',1,'ampl',ampl);
+% mysaveas(pathname,'eff_x',formats,renderer);
+% 
+% plotSolution(S,u,'sigma',2,'ampl',ampl);
+% mysaveas(pathname,'mom_z',formats,renderer);
 
 u = unfreevector(S,u);
 
@@ -177,3 +220,5 @@ plot(s,S+ampl*u,'compo','MOMZ')
 colorbar
 set(gca,'FontSize',fontsize)
 mysaveas(pathname,'mom_z',formats,renderer);
+
+
