36147c943105f124fdb4cdbf624027ff572fb84c
Florent Pled <florent.pled@univ-eiffel.fr>
2025-11-29 00:12:20 +0100

    Update funoptimPF.m

--- examples/phasefield/funoptimPF.m
+++ examples/phasefield/funoptimPF.m
@@ -1,5 +1,5 @@
-function [f,g,h] = funoptimPF(d,A,b,varargin)
-% function [f,g,h] = funoptimPF(d,A,b,varargin)
+function [f, g, H] = funoptimPF(d,A,b,varargin)
+% function [f, g, h] = funoptimPF(d,A,b,varargin)
 
 % Compute objective function
 f = 1/2*d'*A*d - d'*b;
@@ -7,9 +7,9 @@ f = 1/2*d'*A*d - d'*b;
 if nargout > 1 % gradient required
     % Compute gradient of objective function
     g = A*d - b;
-end
-
-if nargout > 2 % hessian required
-    % Compute hessian of objective function
-    h = A;
+    
+    if nargout > 2 % Hessian required
+        % Compute Hessian of objective function
+        H = A;
+    end
 end
